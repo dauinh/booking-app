afcfe9a0c27575a23b56490bdddba7ff64f00f91
linh <linh.ha.nguyen20@gmail.com>
2023-06-09 14:26:49 +0700

    fix connection issue. set up frontend component

---
+++ client/src/components/guest.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Booking App</h1>
+      {/* Add any additional content or components as needed */}
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/src/components/host.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div>
+      <h1>Host Dashboard</h1>
+      {/* Add any additional content or components as needed */}
+    </div>
+  );
+};
+
+export default Dashboard;
