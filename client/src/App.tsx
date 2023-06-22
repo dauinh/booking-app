3a2d29283e1e46b8eef8470a3e46979d881c19a9
linh <linh.ha.nguyen20@gmail.com>
2023-06-22 16:29:20 +0700

    fix frontend routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import HostPage from './pages/host.page';
 import GuestPage from './pages/guest.page';
 import LoginPage from './pages/login.page';
@@ -9,13 +9,11 @@ import LoginPage from './pages/login.page';
 
 function App() {
   return (
-  <Router>
     <Routes>
-      <Route index element={<App />} />
       <Route path="login" element={<LoginPage />} />
       <Route path="host/profile" element={<HostPage />} />
     </Routes>
-  </Router>)
+  )
 }
 
 export default App
